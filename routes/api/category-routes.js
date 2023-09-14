09b0624299a559135be141219d3db05efb69e17c
Noah Burton <nburton115@gmail.com>
2023-09-14 10:52:18 -0600

    category by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,8 +17,18 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try{
+    const categoryById = await Category.findByPk(req.params.id, {include: Product});
+    if (!categoryById) {
+      res.status(404).json({ message: 'No Category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryById);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
