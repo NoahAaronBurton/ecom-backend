1d21ac196e5814138579619012686b22231ab325
Noah Burton <nburton115@gmail.com>
2023-09-18 14:36:38 -0600

    tag put and post

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,12 +31,32 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      },
+    });
+    if (!updateTag[0]) {
+      res.status(404).json({message: 'No Tag by this id!'});
+      return
+    }
+    res.status(200).json(updateTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.delete('/:id', (req, res) => {
